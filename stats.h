9bcd1fbc89678531cc867f5c20534e5605c90e99
Madhav Gentela <madhaveswer@gmail.com>
2022-02-13 15:15:40 -0600

    Creating initial Coursera Repository

--- stats.h
+++ stats.h
@@ -1,10 +1,11 @@
 /******************************************************************************
- * Copyright (C) 2022 by Madhav Gentela - Student Coursera 
+ * Copyright (C) 2017 by Alex Fosdick - University of Colorado
  *
  * Redistribution, modification or use of this software in source or binary
  * forms is permitted as long as the files maintain this copyright. Users are 
  * permitted to modify this and use it to learn about the field of embedded
- * software. Madhav Gentela is not liable for any misuse of this material. 
+ * software. Alex Fosdick and the University of Colorado are not liable for any
+ * misuse of this material. 
  *
  *****************************************************************************/
 /**
