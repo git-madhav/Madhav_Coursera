a2aecda4d2f9cc27044e3573b4efd12a1db6eb2f
Madhav Gentela <madhaveswer@gmail.com>
2022-02-13 15:07:54 -0600

    Creating initial Coursera Repository

---
+++ stats.c
@@ -0,0 +1,42 @@
+/******************************************************************************
+ * Copyright (C) 2022 by Madhav Gentela - Student Coursera 
+ *
+ * Redistribution, modification or use of this software in source or binary
+ * forms is permitted as long as the files maintain this copyright. Users are 
+ * permitted to modify this and use it to learn about the field of embedded
+ * software. Madhav Gentela is not liable for any misuse of this material. 
+ *
+ *****************************************************************************/
+/**
+ * @file stats.c
+ * @brief Function definitions and main.
+ *
+ * This file calculates the basic statistics of a given data and prints on to the screen.
+ *
+ * @author Madhav Gentela
+ * @date 02/13/2022
+ *
+ */
+
+
+
+#include <stdio.h>
+#include "stats.h"
+
+/* Size of the Data Set */
+#define SIZE (40)
+
+void main() {
+
+  unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
+                              114, 88,   45,  76, 123,  87,  25,  23,
+                              200, 122, 150, 90,   92,  87, 177, 244,
+                              201,   6,  12,  60,   8,   2,   5,  67,
+                                7,  87, 250, 230,  99,   3, 100,  90};
+
+  /* Other Variable Declarations Go Here */
+  /* Statistics and Printing Functions Go Here */
+
+}
+
+/* Add other Implementation File Code Here */
---
+++ stats.h
@@ -0,0 +1,39 @@
+/******************************************************************************
+ * Copyright (C) 2022 by Madhav Gentela - Student Coursera 
+ *
+ * Redistribution, modification or use of this software in source or binary
+ * forms is permitted as long as the files maintain this copyright. Users are 
+ * permitted to modify this and use it to learn about the field of embedded
+ * software. Madhav Gentela is not liable for any misuse of this material. 
+ *
+ *****************************************************************************/
+/**
+ * @file stats.h
+ * @brief Function declerations.
+ *
+ * This file describes all the functions needed in stats.c for the assignment. 
+ *
+ * @author Madhav Gentela
+ * @date 02/13/2022
+ *
+ */
+#ifndef __STATS_H__
+#define __STATS_H__
+
+/* Add Your Declarations and Function Comments here */ 
+
+/**
+ * @brief <Add Brief Description of Function Here>
+ *
+ * <Add Extended Description Here>
+ *
+ * @param <Add InputName> <add description here>
+ * @param <Add InputName> <add description here>
+ * @param <Add InputName> <add description here>
+ * @param <Add InputName> <add description here>
+ *
+ * @return <Add Return Informaiton here>
+ */
+
+
+#endif /* __STATS_H__ */
